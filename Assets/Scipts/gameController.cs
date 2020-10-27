6d7f930b1f730303b6e21b2fd131248ecf9eb1a3
siddiq <msm2029@gmail.com>
2020-10-27 14:35:52 +0400

    added ball and spawnner

---
+++ Assets/Scipts/gameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameController : MonoBehaviour
+{
+    public GameObject ball;
+    public Transform spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnBall();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            spawnBall();
+        }
+        
+    }
+
+    void spawnBall()
+    {
+        Instantiate(ball, spawnPoint.position, Quaternion.identity);
+    }
+}
